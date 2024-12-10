0cbbd468cf5cb1fc6ac7f58ddb958c9c3dfe572c
HargunS1 <136589718+HargunS1@users.noreply.github.com>
2024-12-09 20:54:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ const express = require('express');
 const config = require('config');
 const routes = require('./routes');
 const logMiddleware = require('./middlewares/logMiddleware');
+const promClient = require('prom-client'); // For Prometheus metrics
 
 const app = express();
 app.use(express.json());
 app.use(logMiddleware);
+
+// Prometheus metrics setup
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register }); // Collect default Node.js metrics
+
+// Custom HTTP request counter
+const httpRequestCounter = new promClient.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+});
+
+// Middleware to increment request counter
+app.use((req, res, next) => {
+    httpRequestCounter.inc();
+    next();
+});
+
+// Expose Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
 
 // Load configurations
 const port = config.get('port');
@@ -32,4 +55,3 @@ app.use('/', routes);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
