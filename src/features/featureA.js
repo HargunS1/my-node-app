b8af05175726b1839aea81479ee6f748ad252ab0
HargunS1 <136589718+HargunS1@users.noreply.github.com>
2024-12-14 11:15:32 -0500

    Update featureA.js

--- src/features/featureA.js
+++ src/features/featureA.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Feature A is enabled');
+    res.send('Feature A is enabled : Develope Branch');
 });
 
 module.exports = router;
